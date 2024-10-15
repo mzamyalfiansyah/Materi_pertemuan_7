922d1555723969f1fa85dae232fd4eb8d9c9c1ed
mzamyalfiansyah <111264565+mzamyalfiansyah@users.noreply.github.com>
2024-10-15 14:23:49 +0700

    Update latihan_7.cpp

--- latihan_7.cpp
+++ latihan_7.cpp
@@ -3,23 +3,53 @@ using namespace std;
 
 int main(){
 
-    int angka[10];
-    int n = sizeof(angka) / sizeof(angka[0]);
+    int angka[5];
+    int size;
+    int valuebesar;
+    int value;
 
-    cout << "Masukkan 5 nilai Mahasiswa" << endl;
-    for(int i = 0; i < 10; ++i){
 
-    cout << "Masukkan nilai ke-" << i << ":";
-    cin >> angka[i];
+    cout << "Masukkan 5 nilai :" << endl;
+    for(int i = 1; i < 6; ++i){
+
+        cout << "Masukkan nilai ke-" << i << ":";
+        cin >> angka[i];
     }
-
-    sort(angka, angka + n, greater<int>());
-
+    
+    int terbesar = angka[5];
+    for (int i = 1; i < 6; i++) {
+        if (angka[i] > terbesar) {
+            terbesar = angka[i];
+        }
+    }
+    cout << "Nilai tertinggi adalah: " << terbesar << endl;
+    
     cout << endl;
-    cout << "Angka terurut dari terbesar ke terkecil: ";
-    for (int j = 0; j < n; j++) {
-        cout << angka[j] << " ";
+    
+    int cari;
+    cout << "Masukkan angka untuk diperiksa: ";
+    cin >> cari;
+    
+    cout << endl;
+    
+    bool found = false;
+    for (int i = 1; i < 6; i++) {
+        if (angka[i] == cari) {
+            cout << "nilai " << cari << " ditemukan, di urutan ke " << i << endl;
+            found = true;
+        }
     }
+    
+    if(!found){
+        
+        cout << "nilai " << cari << " tidak ditemukan" << endl;
+            
+            
+    }  
+    
+
+ 
+
     return 0;
 
 }
